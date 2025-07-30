7ac8f67cf3abb7cb54403b2f15dd50dfb37d0591
Soham <sohambhogaonkar20@gmail.com>
2025-07-31 00:00:34 +0530

    EvenOddNum.cpp

---
+++ EvenOddNum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int a;
+int main() {
+    cout<<"Enter a number :";
+    cin>>a;
+    if (a % 2 == 0) {
+        cout<<"The number is even.";
+    }
+    else {
+        cout<<"The number is odd.";
+    }
+return 0;
+}
+/* OUTPUT
+Enter a number :093
+The number is odd. */
